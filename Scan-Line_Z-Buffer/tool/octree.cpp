42f51364be5b921dfeb7bf44d0c145ba00531e55
Fu Renhong <793613936@qq.com>
2022-11-17 20:21:41 +0800

    octree: Complete data insertion

--- Scan-Line_Z-Buffer/tool/octree.cpp
+++ Scan-Line_Z-Buffer/tool/octree.cpp
@@ -1,39 +1,66 @@
 #include "octree.h"
 
+Octree::Octree() {
+	is_leaf = true;
+	children.resize(0);
+}
+
 Octree::Octree(Bound_Box const& _box, int _depth) {
 	box = _box;
 	depth = _depth;
 	is_leaf = true;
-	for (int i = 0; i < 8; i++) {
-		children[i] = nullptr;
-	}
+	children.resize(0);
 }
 
-Octree::~Octree() {
+void Octree::split() {
 	for (int i = 0; i < 8; i++) {
-		if (children[i] != nullptr)
-			delete children[i];
+		Bound_Box new_box;
+		new_box.center = box.center;
+		new_box.center.x += box.half_length.x * (i & 4 ? 0.5f : -0.5f);
+		new_box.center.y += box.half_length.y * (i & 2 ? 0.5f : -0.5f);
+		new_box.center.z += box.half_length.z * (i & 1 ? 0.5f : -0.5f);
+		new_box.half_length = box.half_length * 0.5f;
+		children.push_back(Octree(new_box, depth + 1));
 	}
+	is_leaf = false;
 }
 
-Octree::Octree(Octree const& other)
-	: box(other.box), depth(other.depth), is_leaf(other.is_leaf), data(other.data) {
-	for (int i = 0; i < 8; i++) {
-		children[i] = other.children[i];
-	}
-}
+void Octree::insert(Node_Data const& n) {
+	if (is_leaf) {
+		data.push_back(n);
 
-Octree& Octree::operator=(Octree const& other) {
-	box = other.box;
-	depth = other.depth;
-	is_leaf = other.is_leaf;
-	data = other.data;
-	for (int i = 0; i < 8; i++) {
-		children[i] = other.children[i];
+		if (data.size() > max_data_num) {
+			//depth judge
+			if (depth < max_depth) {
+				split();
+				for (auto& old_data: data) {
+					for (int i = 0; i < 8; i++) {
+						if (is_intersect(children[i].box, old_data.t)) {
+							children[i].insert(old_data);
+						}
+					}
+				}
+				int child_data_num = 0;
+				for (int i = 0; i < 8; i++) {
+					child_data_num += children[i].data.size();
+				}
+				if (child_data_num == 8 * data.size()) {
+					children.resize(0);
+					is_leaf = true;
+				} else {
+					data.resize(0);
+				}
+			}
+		}
+	} else {
+		for (int i = 0; i < 8; i++) {
+			if (is_intersect(children[i].box, n.t)) {
+				children[i].insert(n);
+			}
+		}
 	}
-	return *this;
+	return;
 }
-
 
 Octree* Octree_Constructor::construct(std::vector<int>& triangle_indexes, std::vector<glm::vec3>& vertices) {
 	Octree* root;
@@ -55,7 +82,18 @@ Octree* Octree_Constructor::construct(std::vector<int>& triangle_indexes, std::v
 	}
 	root = new Octree(root_box, 0);
 
-	//todo
+	int triangle_num = triangle_indexes.size() / 3;
+	for (auto i = 0; i < triangle_num; i++) {
+		int _a = triangle_indexes[3 * i];
+		int _b = triangle_indexes[3 * i + 1];
+		int _c = triangle_indexes[3 * i + 2];
+		Node_Data node;
+		node.id = i;
+		node.t.a = vertices[_a];
+		node.t.b = vertices[_b];
+		node.t.c = vertices[_c];
+		root->insert(node);
+	}
 
 	return root;
 }
--- Scan-Line_Z-Buffer/tool/octree.h
+++ Scan-Line_Z-Buffer/tool/octree.h
@@ -3,22 +3,27 @@
 #include <vector>
 #include "intersect.h"
 
+struct Node_Data {
+	Triangle t;
+	size_t id;
+};
+
 struct Octree {
+	Octree();
 	Octree(Bound_Box const& _box, int _depth);
-	~Octree();
-	Octree(Octree const& other);
-	Octree& operator=(Octree const& other);
+	void split();
+	void insert(Node_Data const& n);
 
 	Bound_Box box;
 	int depth;
 	bool is_leaf;
-	Octree* children[8];
-	std::vector<size_t> data; // id pointing to triangle
+	std::vector<Octree> children; // size = 8
+	std::vector<Node_Data> data; // id pointing to triangle
+
+	const int max_depth = 7;
+	const int max_data_num = 5;
 };
 
 struct Octree_Constructor {
-public:
 	Octree* construct(std::vector<int>& triangle_indexes, std::vector<glm::vec3>& vertices);
-private:
-	const int max_depth = 10;
 };
