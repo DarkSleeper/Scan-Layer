7b29548308bf11a69a6678a5f6936deeb81dd458
Fu Renhong <793613936@qq.com>
2022-11-19 21:51:55 +0800

    mipmap: Move get_far_z public

--- Scan-Line_Z-Buffer/tool/mipmap.h
+++ Scan-Line_Z-Buffer/tool/mipmap.h
@@ -5,10 +5,10 @@
 struct MipMap {
 public:
 	MipMap(int screen_width, int screen_height, const float* level_0_buffer);
+	float get_far_z(glm::vec2 rec_min, glm::vec2 rec_max);
 
 private:
 	void init_data();
-	float get_far_z(glm::vec2 rec_min, glm::vec2 rec_max);
 	std::vector<std::vector<std::vector<float>>> datas;
 	int level;
 	int max_width;
